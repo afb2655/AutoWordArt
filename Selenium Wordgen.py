e2a3c3aca046354140d5e32cd05fdae555d6f58c
theaiden28 <31458159+theaiden28@users.noreply.github.com>
2018-02-27 07:25:25 -0500

    Program sort of works, needs some work on the screenshot

---
+++ Selenium Wordgen.py
@@ -0,0 +1,53 @@
+from datetime import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+
+def mainv1(nicktext):
+    browser = webdriver.Chrome()
+    browser.get('https://worditout.com/word-cloud/create')
+    button = browser.find_element_by_name('paste')
+    button.click()
+    button.send_keys(nicktext)
+    print(button)
+    Generate = browser.find_element_by_css_selector(".c.bz.cY.S.bU.eg.cu")
+    Generate.click()
+    browser.fullscreen_window()
+    browser.save_screenshot("nickposting.png")
+    Save = browser.find_element_by_css_selector(".c.S.i.co.aU")
+    Save.click()
+    Title = browser.find_element_by_name("title")
+    Title.send_keys("Nick's Autism")
+    Description = browser.find_element_by_name("description")
+    Description.send_keys("An unholy Rambleing")
+    email = browser.find_element_by_name("email")
+    email.send_keys('a1597579@nwytg.com')
+    email.submit()
+
+
+def tagcrowdart():
+    browser = webdriver.Chrome()
+    browser.get('https://tagcrowd.com/')
+    button = browser.find_element_by_name('text')
+    button.click()
+    button.send_keys("NBC’s Katie Couric suggested during coverage of the Opening Ceremony, there is the fact that the Dutch consider speedskating to be “an important mode of transport” over frozen canals. Damn I want to be a news anchor I've given up on wining over any girls in person with my non existent personality, and switched exclusively to tinder. I still have yet to create a profile, not being sure what pics to use. I don't want to just slap something on there but I have no idea what would be a flattering and well thought out shot. I will however include the picture of me making frens with a horse after that in my profile. Laurance, please do not subvert yourself for others pointless causes. A man deserves to live his life, making his own choices, embracing his brothers truly, and loving his family wholeheartedly. This false cause of 'cucks' and 'feminism' is only an effort to steal this from you, to make you fight your brothers and those you love, for the enrichment of the bastards who care not for you or your dreams. Please do not let such things limit you. Do not avoid living your life the best you can out of fear of 'nu-males' and 'degeneracy'. Do not fear to express and live your love because of how those still asleep will view you. The hypothetical men in your pictures are just the same as you. They had dreams, they had wives they loved, but they threw these away because they had been manipulated into seeing no other choice, because they feared, and thus could not think to fight for what is theirs. Just as you fear to share your talent for cooking with the girl you care about, and fear to feel, that you be seen as weak and unmanly. Trust me, none of what others say matters. I've lived for 18 years trying to be something for others, afraid to even have a hobby. This is where such ideologies lead, to being a man who looks back and realizes the happiness  he could have had, and the dreams he's allowed to languish. I know it's hard to wake up to these things, but you'll be a man soon, and hard as it is, finding your desires and pursuing them is what makes a man.")
+    print(button)
+    Visualize = browser.find_element_by_name('tc_submit')
+    Visualize.submit()
+    browser.fullscreen_window()
+    browser.save_screenshot("nickposting.png")
+
+
+if __name__ == '__main__':
+    file = open('nickposts.txt','r')
+    stringaccum = ""
+    for line in file:
+        temp = line.strip().split(':')
+        stringaccum += temp[3]
+
+    mainv1(stringaccum)
+
+
+
